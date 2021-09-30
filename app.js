6dcc9584e258ea7ab286d342da11c8b760751157
sofiaperezm <sofi.prz.m@gmail.com>
2021-09-30 01:06:00 -0500

    added createBadge function

--- app.js
+++ app.js
@@ -1,9 +1,45 @@
 import jobsData from "./data.js";
-console.log(jobsData[0])
 
+const container = document.querySelector(".container")
 
-function createCard() {
-    console.log("holi")
+jobsData.forEach(element => {
+    const cardElement = createCard(element)
+    container.appendChild(cardElement)
+});
+
+function createCard(job) {
+    const cardElement = document.createElement("div");
+    const badges = createBadge(job.new, job.featured);
+    badges.forEach(badge => {
+        cardElement.appendChild(badge)
+    })
+    return cardElement
 }
 
-console.log(createCard())
+function createBadge(isNew, isFeatured) {
+    if (!isNew && !isFeatured) {
+        return [];
+    }
+    
+    const badges = [];
+    if (isNew) {
+        const newBadge = document.createElement("p");
+        newBadge.innerText = "New!"
+        newBadge.classList.add(["badge", "badge--new"]);
+        badges.push(newBadge)
+    }
+
+    if (isFeatured) {
+        const featuredBadge = document.createElement("p");
+        featuredBadge.innerText = "Featured"
+        featuredBadge.classList.add(["badge", "badge--featured"]);
+        badges.push(featuredBadge)
+    }
+
+    return badges
+}
+
+console.log(createBadge(true, true))
+console.log(createBadge(true, false))
+console.log(createBadge(false, false))
+console.log(createBadge(false, true))
